28fea1647f75629af7e54051d968fdbde1728708
Jason Shiverick <jshiverick@rivian.coml>
2022-01-27 10:56:03 -0800

    Added letter not in position logic

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.17
 
 require (
 	github.com/briandowns/spinner v1.18.0
+	github.com/dustin/go-humanize v1.0.0
 	github.com/fatih/color v1.7.0
 	github.com/manifoldco/promptui v0.9.0
 )
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"embed"
+	"errors"
 	"fmt"
 	"os"
 	"runtime"
@@ -9,6 +10,7 @@ import (
 	"time"
 
 	"github.com/briandowns/spinner"
+	"github.com/dustin/go-humanize"
 	"github.com/manifoldco/promptui"
 
 	"github.com/fatih/color"
@@ -19,6 +21,7 @@ var sgb_words string
 
 var fs embed.FS
 
+// knownLetters: letters are known to be in a specific position
 type knownLetters struct {
 	firstLetter  string
 	secondLetter string
@@ -27,17 +30,46 @@ type knownLetters struct {
 	fifthLetter  string
 }
 
+// knownNotLetters: letters are known to not be in a specific position
+type knownIsNotLetters struct {
+	firstLetterIsNot  string
+	secondLetterIsNot string
+	thridLetterIsNot  string
+	fourthLetterIsNot string
+	fifthLetterIsNot  string
+}
+
 func main() {
 
+	var knl knownIsNotLetters
 	sl := strings.Split(string(sgb_words), "\n")
 
 	color.Cyan("GO WORDLE")
-	lettersHas, lettersHasNot, kl := getPrompt()
+	guesses, lettersHas, lettersHasNot, kl := getPrompt()
+	for _, guess := range guesses {
+		knl.getNotPositions(guess, kl, lettersHas)
+	}
+	if knl.firstLetterIsNot != "" {
+		color.Cyan("1st letter is not: " + knl.firstLetterIsNot)
+	}
+	if knl.secondLetterIsNot != "" {
+		color.Cyan("2nd letter is not: " + knl.secondLetterIsNot)
+	}
+	if knl.thridLetterIsNot != "" {
+		color.Cyan("3rd letter is not: " + knl.thridLetterIsNot)
+	}
+	if knl.fourthLetterIsNot != "" {
+		color.Cyan("4th letter is not: " + knl.fourthLetterIsNot)
+	}
+	if knl.fifthLetterIsNot != "" {
+		color.Cyan("5th letter is not: " + knl.fifthLetterIsNot)
+	}
+
 	s := spinner.New(spinner.CharSets[26], 100*time.Millisecond) // Build our new spinner
 	s.Start()                                                    // Start the spinner
 	time.Sleep(2 * time.Second)                                  // Run for some time to simulate work
 	s.Stop()
-	hasWords := hasWordle(sl, lettersHas, lettersHasNot, kl)
+	hasWords := hasWordle(sl, lettersHas, lettersHasNot, kl, knl)
 	if len(hasWords) == 0 {
 		color.Red("No Words :-|")
 		os.Exit(0)
@@ -45,11 +77,11 @@ func main() {
 
 }
 
-func hasWordle(words []string, lettersHas []string, lettersNot string, kl knownLetters) []string {
+func hasWordle(words []string, lettersHas []string, lettersNot string, kl knownLetters, knl knownIsNotLetters) []string {
 	color.Cyan("WORDLE Try...")
 	var hasWords []string
 	for _, s := range words {
-		if hasChars(s, lettersHas) && (strings.ContainsAny(s, lettersNot) == false) && hasPositions(s, kl) {
+		if hasChars(s, lettersHas) && !strings.ContainsAny(s, lettersNot) && hasPositions(s, kl, knl) {
 			if runtime.GOOS == "windows" {
 				fmt.Println(s)
 			} else {
@@ -62,7 +94,33 @@ func hasWordle(words []string, lettersHas []string, lettersNot string, kl knownL
 	return hasWords
 }
 
-func getPrompt() ([]string, string, knownLetters) {
+func getPrompt() ([]string, []string, string, knownLetters) {
+
+	var guesses []string
+	for i := 1; i <= 6; i++ {
+		validateGuess := func(input string) error {
+			if len(input) != 5 && input != "" {
+				return errors.New("Wordle Guess should be 5 letters")
+			}
+			return nil
+		}
+
+		pg := promptui.Prompt{
+			Label:    humanize.Ordinal(i) + " Guess",
+			Default:  "",
+			Validate: validateGuess,
+		}
+
+		guess, err := pg.Run()
+		if guess == "" {
+			break
+		}
+		guesses = append(guesses, guess)
+
+		if err == promptui.ErrInterrupt {
+			os.Exit(0)
+		}
+	}
 
 	pa := promptui.Prompt{
 		Label:   "Wordle has the letters",
@@ -173,7 +231,7 @@ func getPrompt() ([]string, string, knownLetters) {
 		fmt.Printf("Prompt failed %v\n", err)
 	}
 
-	return lettersHas, lettersHasNot, knownLetters{firstLetter: l1, secondLetter: l2, thridLetter: l3, fourthLetter: l4, fifthLetter: l5}
+	return guesses, lettersHas, lettersHasNot, knownLetters{firstLetter: l1, secondLetter: l2, thridLetter: l3, fourthLetter: l4, fifthLetter: l5}
 
 }
 
@@ -185,28 +243,99 @@ func hasChars(s string, chars []string) bool {
 	return b
 }
 
-func hasPositions(s string, kl knownLetters) bool {
+func hasPositions(word string, kl knownLetters, knl knownIsNotLetters) bool {
 	b := true
 	if kl.firstLetter != "" {
-		b = b && (string(s[0]) == kl.firstLetter)
+		c := string(word[0])
+		b = b && (c == kl.firstLetter)
+	}
+
+	if knl.firstLetterIsNot != "" {
+		c := string(word[0])
+		b = b && (c != knl.firstLetterIsNot)
 	}
 
 	if kl.secondLetter != "" {
-		b = b && (string(s[1]) == kl.secondLetter)
+		c := string(word[1])
+		b = b && (c == kl.secondLetter)
+	}
+
+	if knl.secondLetterIsNot != "" {
+		c := string(word[1])
+		b = b && (c != knl.secondLetterIsNot)
 	}
 
 	if kl.thridLetter != "" {
-		b = b && (string(s[2]) == kl.thridLetter)
+		c := string(word[2])
+		b = b && (c == kl.thridLetter)
+	}
+
+	if knl.thridLetterIsNot != "" {
+		c := string(word[2])
+		b = b && (c != knl.thridLetterIsNot)
 	}
 
 	if kl.fourthLetter != "" {
-		b = b && (string(s[3]) == kl.fourthLetter)
+		c := string(word[3])
+		b = b && (c == kl.fourthLetter)
+	}
+
+	if knl.fourthLetterIsNot != "" {
+		c := string(word[3])
+		b = b && (c != knl.fourthLetterIsNot)
 	}
 
 	if kl.fifthLetter != "" {
-		b = b && (string(s[4]) == kl.fifthLetter)
+		c := string(word[4])
+		b = b && (c == kl.fifthLetter)
+	}
+
+	if knl.fifthLetterIsNot != "" {
+		c := string(word[4])
+		b = b && (c != knl.fifthLetterIsNot)
 	}
 
 	return b
+
+}
+
+func (knl *knownIsNotLetters) getNotPositions(guess string, kl knownLetters, hasLetters []string) {
+
+	for _, l := range hasLetters {
+		hasLetterIndex := strings.Index(guess, l)
+		switch hasLetterIndex {
+		case -1:
+			continue
+		// the first letter we know of
+		case 0:
+			// does not match the position we know it is in
+			if kl.firstLetter != l {
+				knl.firstLetterIsNot = l
+			}
+		case 1:
+
+			if kl.secondLetter != l {
+				knl.secondLetterIsNot = l
+			}
+
+		case 2:
+
+			if kl.thridLetter != l {
+				knl.thridLetterIsNot = l
+			}
+
+		case 3:
+
+			if kl.fourthLetter != l {
+				knl.fourthLetterIsNot = l
+			}
+
+		case 4:
+
+			if kl.fifthLetter != l {
+				knl.fifthLetterIsNot = l
+			}
+		}
+	}
 
 }
